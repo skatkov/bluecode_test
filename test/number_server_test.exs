9b38c3c88cfb9ab7c225915d1c2252f998c4a709
Stanislav K <lunaticman@gmail.com>
2019-01-22 01:10:46 +0700

    Adding NumberServer#checksum method
    trying to finish calculation of checksum
    
    now it calculates Checksum

---
+++ test/number_server_test.exs
@@ -0,0 +1,31 @@
+defmodule ApiServer.NumberServerTest do
+	use ExUnit.Case
+
+  test "appends numbers" do
+  	{:ok, _} = GenServer.start_link(NumberServer, [], name: NumberServer)
+
+  	assert NumberServer.get == []
+  	NumberServer.append([1, 2, 3])
+  	assert NumberServer.get == [1, 2, 3]
+  	NumberServer.append([9, 8, 7])
+  	assert NumberServer.get == [1, 2, 3, 9, 8, 7]
+  end
+
+  test 'clean up state' do
+  	{:ok, _} = GenServer.start_link(NumberServer, [], name: NumberServer)
+
+  	NumberServer.append([1,2,3])
+  	NumberServer.clear
+
+  	assert NumberServer.get == []
+  end
+
+
+  test 'checksum' do
+  	{:ok, _} = GenServer.start_link(NumberServer, [], name: NumberServer)
+
+  	NumberServer.append([5, 4, 8, 9, 8, 5, 0, 3, 5, 4])
+
+  	assert NumberServer.checksum() == 7
+  end
+end
